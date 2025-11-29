d39a5607e00058ea876e5608faaa612b8ea78091
sheshikumar1 <sheshikumarerraboina@gmail.com>
2025-11-29 13:26:39 +0530

    bookstore added

---
+++ bookstore/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+app=FastAPI()
+
+@app.get("/book/{book_id}")
+async def read_book(book_id: int):
+    return {
+        "book_id":book_id,
+        "title":"the great india",
+        "author":"sheshi"
+    }
+
+@app.get("/authors/{author_id}")
+async def read_author(author_id: int):
+    return {
+        'author_id':author_id,
+        'name':"sheshi"
+    }
+
+@app.get("/books")
+async def read_books(year: int=None):
+    if year:
+        return {
+            'year':year,
+            'books':['Book 1','Book 2']
+        }
+    return {'books':["ALL Books"]}
