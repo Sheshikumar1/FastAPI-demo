310e7a3879480a42cd0d66058e5078924166452e
sheshikumar1 <sheshikumarerraboina@gmail.com>
2025-12-13 15:58:24 +0530

    error

--- bookstore/main.py
+++ bookstore/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from starlette.responses import JSONResponse
 from models import Book
 from pydantic import BaseModel
 app=FastAPI()
@@ -48,3 +49,16 @@ async def read_all_books() ->list[BooKResponse]:
             "author":"F. Scott Fitzgerald"
         },
     ]
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "message":"Oops! Something went wrong "
+        },
+    )
+
+@app.get("/error_endpoint")
+async def raise_exception():
+    raise HTTPException(status_code=400)
